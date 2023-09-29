d51f78fbadf72310e118593bd10bb3a1d29982c7
haris <harisrafli0@gmail.com>
2023-09-29 19:58:55 +0700

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,175 @@
+export const Bio = {
+  name: "Haris Rafli Agustin",
+  roles: [
+    "Fresh Graduate",
+    "Junior Web Developer",
+  ],
+  description:
+    "Saya adalah Fresh Graduate Universitas Bina Sarana Informatika, Prodi Sistem Informasi D3.",
+  github: "https://github.com/rishavchanda",
+  resume:
+    "https://drive.google.com/file/d/12jRp5-YKlXEo9FBalQFKAmwkOmSCL_2E/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/harisrafliagustin/",
+  twitter: "https://twitter.com/Haris_Rafli/",
+  insta: "https://www.instagram.com/harisrafli/",
+  facebook: "https://www.facebook.com/Haris.Rafli/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Laravel",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Laravel.svg/985px-Laravel.svg.png",
+      },
+      {
+        name: "Codeigniter",
+        image: "https://cdn-peanutsquare.b-cdn.net/wp-content/uploads/2022/11/codeigniter-logo.png",
+      },
+      {
+        name: "PHP",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/2560px-PHP-logo.svg.png",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+    ],
+  },
+  {
+    title: "MS.Office",
+    skills: [
+      {
+        name: "MS.Word",
+        image: "https://icons.iconarchive.com/icons/carlosjj/microsoft-office-2013/256/Word-icon.png",
+      },
+      {
+        name: "MS.Excel",
+        image:
+          "https://icons.iconarchive.com/icons/carlosjj/microsoft-office-2013/256/Excel-icon.png",
+      },
+      {
+        name: "Ms.PowerPoint",
+        image:
+          "https://icons.iconarchive.com/icons/carlosjj/microsoft-office-2013/256/PowerPoint-icon.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 1,
+    img: "https://www.tagar.id/Asset/uploads/172459-badan-narkotika-nasional.jpeg",
+    role: "Staff",
+    company: "Badan Narkotika Nasional Jakarta Timur",
+    date: "January 2018 - April 2018",
+    desc: "Memiliki pengalaman magang yang cukup baik pada bidang Staff di Badan Narkotika Nasional Jakarta Timur selama 3 bulan.",
+    skills: [
+      "Menginput data surat bebas narkoba",
+      "Mengecek urin",
+      "Membuat laporan",
+    ],
+    doc: "https://firebasestorage.googleapis.com/v0/b/certificate-f7040.appspot.com/o/All%20Sertifikat_page-0001.jpg?alt=media&token=00bebfe8-1303-46f3-997d-fd65a2863f81&_gl=1*srkk0y*_ga*MTYyMzYzMTU5Mi4xNjk1OTAxNDA1*_ga_CW55HF8NVT*MTY5NTkwMTQwNS4xLjEuMTY5NTkwMjYyMS40NS4wLjA.",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://api-frontend.kemdikbud.go.id/v2/detail_pt_logo/MUE4NEE5QUQtNzVGOS00NENELUJFQkEtNUE1RDIzQzdGNjEz",
+    school: "Universitas Bina Sarana Informatika",
+    date: "Sep 2019 - Nov 2022",
+    grade: "3.97 IPK",
+    desc: "Currently I am a Fresh Graduate from Bina Sarana Informatics University. D3 Information Systems Study Program.",
+    degree: "Associate's degree of Information Systems - UBSI.",
+  },
+  {
+    id: 1,
+    img: "https://smkmalaka.sch.id/wp-content/uploads/2011/07/logo-smk.jpg",
+    school: "SMK Malaka",
+    date: "Jan 2016 - Jan 2019",
+    grade: "Not known",
+    desc: "I am a graduate of Malaka Vocational School majoring in Computer Network Engineering.",
+    degree: "(XII), Computer Network Engineering.",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Point Of Sales",
+    date: "Mei 2022 - Jul 2022",
+    description:
+      "Produk point of sales yang standart nantinya dapat digunakan oleh perusahaan untuk di gunakan sebagai alat bantu penjualan barang IT.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/certificate-f7040.appspot.com/o/POS.jpg?alt=media&token=74b0d579-0829-4b4f-962b-d506ebdc0385&_gl=1*rvldzs*_ga*MTYyMzYzMTU5Mi4xNjk1OTAxNDA1*_ga_CW55HF8NVT*MTY5NTkxOTAzNC4yLjEuMTY5NTkxOTQ2OC40OC4wLjA.",
+    tags: ["HTML", "CSS", "Javascript", "Bootstrap", "JQuery", "PHP", "MySQL"],
+    category: "web app",
+    webapp: "",
+    member: [
+      {
+        name: "Haris Rafli Agustin",
+        img: "https://firebasestorage.googleapis.com/v0/b/certificate-f7040.appspot.com/o/HeroImage.jpg?alt=media&token=f913d3f2-d772-4387-8345-dc2caaedc880&_gl=1*nxqqt9*_ga*MTYyMzYzMTU5Mi4xNjk1OTAxNDA1*_ga_CW55HF8NVT*MTY5NTkxOTAzNC4yLjEuMTY5NTkxOTg5NC4xNS4wLjA.",
+        linkedin: "https://www.linkedin.com/in/harisrafliagustin/",
+      },
+    ],
+  },
+];
+
+export const TimeLineData = [
+  { year: 2016, text: "Started my journey" },
+  { year: 2022, text: "Started my project" },
+];

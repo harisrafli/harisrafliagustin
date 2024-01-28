1b6c92eb694e07d77a0e98c45b31863799b5ebb8
Haris Rafli Agustin <146442155+harisrafli@users.noreply.github.com>
2024-01-28 23:17:43 +0700

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -8,7 +8,7 @@ export const Bio = {
     "Saya adalah Fresh Graduate Universitas Bina Sarana Informatika, Prodi Sistem Informasi D3.",
   github: "https://github.com/harisrafli",
   resume:
-    "https://drive.google.com/file/d/18Yd-t-SWVFhk5qHx2DFg2JLCq674n6_P/view?usp=sharing",
+    "https://drive.google.com/file/d/18Yd-t-SWVFhk5qHx2DFg2JLCq674n6_P/view?usp=drive_link",
 };
 
 export const skills = [
